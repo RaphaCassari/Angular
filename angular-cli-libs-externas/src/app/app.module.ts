5a8f19491a3cc86a926bd88a2f144efcd512b787
Raphael Benedicto <raphael.benedicto@grupomemorial.net>
2020-11-03 19:58:01 -0300

    angular2-materialize(rever build)

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 @NgModule({
   declarations: [
     AppComponent
