dbabc46323e32a58b976710bc26fd38b2c798876
Raphael Cassari Benedicto <raphacassari@gmail.com>
2020-12-10 20:03:14 -0300

    Todas Diretivas

--- Diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ Diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,13 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  cursos: string[] = [];
+  cursos: string[] = ["Angular 2"];
 
-  mostrarCursos = false;
+  mostrarCursos: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   onMostrarCursos(){
